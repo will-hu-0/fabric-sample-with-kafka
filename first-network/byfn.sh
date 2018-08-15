4e798195d11380113e3e35b5efabfb78223cfed0
Will Hu <chopin.champagne@gmail.com>
2018-08-15 20:20:50 +0800

    Will-For travis, remove askProcced

--- first-network/byfn.sh
+++ first-network/byfn.sh
@@ -515,7 +515,7 @@ else
   echo "${EXPMODE} for channel '${CHANNEL_NAME}' with CLI timeout of '${CLI_TIMEOUT}' seconds and CLI delay of '${CLI_DELAY}' seconds"
 fi
 # ask for confirmation to proceed
-askProceed
+# askProceed For Travis 
 
 #Create the network using docker compose
 if [ "${MODE}" == "up" ]; then
